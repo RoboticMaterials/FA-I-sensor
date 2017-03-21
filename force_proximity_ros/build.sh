357181d3748a2b674ef4b6bd8faa677d157f172f
Guilherme <guilherme.c.affonso@gmail.com>
2017-03-22 00:13:12 +0900

    Update

--- force_proximity_ros/build.sh
+++ force_proximity_ros/build.sh
@@ -2,4 +2,5 @@
 catkin build
 source devel/setup.bash
 echo Using '~/Arduino/libraries' as sketchbook directory.
+rm -rf ~/Arduino/libraries/ros_lib
 rosrun rosserial_arduino make_libraries.py ~/Arduino/libraries/ force_proximity_ros
